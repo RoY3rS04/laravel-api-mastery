5cec44305b43106a92d32605e6b68bdc63a05324
RoY3rS04 <rogermoises2004@gmail.com>
2026-05-20 15:56:03 -0600

    starting the laravel api master class course from laracasts

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
+            'status' => $this->faker->randomElement(['A', 'C', 'H', 'X']),
+        ];
+    }
+}
