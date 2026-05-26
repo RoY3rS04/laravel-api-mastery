a6f502e3be04a14630330c5928b8605b2d45d271
RoY3rS04 <rogermoises2004@gmail.com>
2026-05-26 11:33:56 -0600

    Replacing Resources with Put Requests

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
@@ -49,6 +50,32 @@ class AuthorTicketsController extends ApiController
                 'Ticket cannot be found',
                 Response::HTTP_NOT_FOUND
             );
+        } catch (ModelNotFoundException $e) {
+            return $this->error(
+                'Ticket cannot be found',
+                Response::HTTP_NOT_FOUND
+            );
+        }
+    }
+
+    public function replace(ReplaceTicketRequest $request, int $author_id, int $ticket_id) {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($ticket->user_id !== $author_id) {
+                // TODO: ticket doesn't belong to user
+            }
+
+            $model = [
+                'title' => $request->input('data.attributes.title'),
+                'description' => $request->input('data.attributes.description'),
+                'status' => $request->input('data.attributes.status'),
+                'user_id' => $request->input('data.relationships.author.data.id'),
+            ];
+
+            $ticket->update($model);
+
+            return new TicketResource($ticket->fresh());
         } catch (ModelNotFoundException $e) {
             return $this->error(
                 'Ticket cannot be found',
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,11 +5,19 @@ use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')
-    ->apiResource('/tickets', TicketController::class);
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::middleware('auth:sanctum')
-    ->apiResource('/authors', AuthorsController::class);
+    Route::apiResource('/tickets', TicketController::class)
+        ->except(['update']);
 
-Route::middleware('auth:sanctum')
-    ->apiResource('/authors.tickets', AuthorTicketsController::class);
+    Route::put('/tickets/{ticket}', [TicketController::class, 'replace']);
+
+    Route::apiResource('/authors', AuthorsController::class);
+
+    Route::apiResource('/authors.tickets', AuthorTicketsController::class)
+        ->except(['update']);
+
+    Route::put('/authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace']);
+});
+
+
